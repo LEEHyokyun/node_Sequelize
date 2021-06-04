eb34103e3cf6da40a4252779cd16adf52598c26b
hyokyun <gyrbs22@gmail.com>
2021-06-05 00:21:53 +0900

    Sequelize and create database by posted data

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,3 +1,5 @@
+const models = require("../../models");
+
 //실질적인 기능을 구현하기 위한 js파일
 exports.get_products = (_, res) => {
   res.render("admin/products.html", { message: "hello" });
@@ -7,6 +9,18 @@ exports.get_products_write = (_, res) => {
   res.render("admin/write.html");
 };
 
+//받은 데이터로 db table 생성하기
+//이전 페이지에서 작성을 눌러 post 메소드를 통해 data를 받음
+//그 data를 받고 Products table에 data를 기재해주는것 = create(생성)
 exports.post_products_write = (req, res) => {
-  res.send(req.body);
+  //data field 맞추기
+  //넘어오는 data는 bodyParser형태!(req.body 형태)
+  //Query = insert , Sequelize = create
+  models.Products.create(
+    req.body
+    //저장하고 main page로 간다
+    //redirect (call back)
+  ).then(() => {
+    res.redirect("/admin/products");
+  });
 };
--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -9,6 +9,8 @@ router.get("/products", ctrl.get_products);
 
 router.get("/products/write", ctrl.get_products_write);
 
+//사용자가 입력한 실질적인 data가 post 요청을 통해 받아와진다
+//index.js는 함수만 기재(실질 기능은 controller에서 구현)
 router.post("/products/write", ctrl.post_products_write);
 
 module.exports = router;
