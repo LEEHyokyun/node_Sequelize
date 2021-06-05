07ac162653421b3f9bc1448aa14ae4dbdff26f54
hyokyun <gyrbs22@gmail.com>
2021-06-06 00:00:12 +0900

    clean code by async await

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,13 +1,17 @@
 const models = require("../../models");
 
 //실질적인 기능을 구현하기 위한 js파일
-exports.get_products = (_, res) => {
+exports.get_products = async (_, res) => {
   //받은 데이터로 Products database 조회하기
   //data 조건에 맞춘 조회(Query의 where절)
   //변수를 productsList로 정하고 여기에 data들이 저장됨
-  models.Products.findAll({}).then((productList) => {
+  try {
+    const productList = await models.Products.findAll();
     res.render("admin/products.html", { productList: productList });
-  });
+    /*models.Products.findAll({}).then((productList) => {
+    res.render("admin/products.html", { productList: productList });*/
+    //변수에 맞게
+  } catch (e) {}
 };
 
 exports.get_products_write = (_, res) => {
@@ -17,17 +21,20 @@ exports.get_products_write = (_, res) => {
 //받은 데이터로 db table 생성하기
 //이전 페이지에서 작성을 눌러 post 메소드를 통해 data를 받음
 //그 data를 받고 Products table에 data를 기재해주는것 = create(생성)
-exports.post_products_write = (req, res) => {
+exports.post_products_write = async (req, res) => {
   //data field 맞추기
   //넘어오는 data는 bodyParser형태!(req.body 형태)
   //Query = insert , Sequelize = create
-  models.Products.create(
+
+  await models.Products.create(req.body);
+  res.redirect("/admin/products");
+  /*models.Products.create(
     req.body
     //저장하고 main page로 간다
     //redirect (call back)
   ).then(() => {
     res.redirect("/admin/products");
-  });
+  });*/
 };
 
 //상세페이지
